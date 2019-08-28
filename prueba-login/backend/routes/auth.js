0a2802f9a09617d852f8180cd3ae4d81d2ca25ca
Javier Duarte <javierandresduarte@gmail.com>
2019-08-28 13:57:47 -0500

    login api

--- prueba-login/backend/routes/auth.js
+++ prueba-login/backend/routes/auth.js
@@ -5,10 +5,9 @@ router.route('/add').post((req,res)=>{
     const username = req.body.username
     const password = req.body.password
 
-    const newUser = new User({
-        username: username,
-        password: password
-    })
+    const newUser = new User()
+    newUser.username = username
+    newUser.password = newUser.generateHash(password)
 
     newUser.save()
         .then(users=>{
@@ -23,6 +22,44 @@ router.route('/').get((req,res)=>{
     User.find()
         .then(users=>{
             res.json(users)
+        })
+        .catch(err=>res.status(400).json('Error: '+ err))
+})
+
+router.route('/login').post((req,res)=>{
+    const username = req.body.username
+    const password = req.body.password
+
+    User.find({
+        username:username
+    })
+        .then(users=>{
+
+            if(users.length==0){
+                return res.send({
+                    success:false,
+                    message: 'Error: usuario no encontrado'
+                })
+            }
+
+            if(users.length>1){
+                return res.send({
+                    success:false,
+                    message: 'Error: usuario inválido'
+                })
+            }
+
+            if(users[0].validPassword(password)){
+                return res.send({
+                    success:true,
+                    message: 'Ha iniciado sesión'
+                })
+            } else{
+                return res.send({
+                    success:false,
+                    message: 'Error: La contraseña es incorrecta'
+                })
+            }
         })
         .catch(err=>res.status(400).json('Error: '+ err))
 })
